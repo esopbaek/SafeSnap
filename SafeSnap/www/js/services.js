8656426eebb91b9792be84e5cf6a1565cf639214
Esop Baek <e.baek@celmatix.com>
2016-08-06 04:03:55 -0400

    set relationship setup

--- SafeSnap/www/js/services.js
+++ SafeSnap/www/js/services.js
@@ -9,31 +9,35 @@ angular.module('safeSnap.services', [])
     name: 'Ben Sparrow',
     setCount: 3,
     lastText: 'You on your way?',
-    face: 'img/ben.png'
+    face: 'img/profile_photos/ben.png',
+    sets: [{
+      name: "Foot",
+      
+    }]
   }, {
     id: 1,
     name: 'Max Lynx',
     setCount: 2,
     lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    face: 'img/profile_photos/max.png'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     setCount: 4,
     lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    face: 'img/profile_photos/adam.jpg'
   }, {
     id: 3,
     name: 'Perry Governor',
     setCount: 1,
     lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    face: 'img/profile_photos/perry.png'
   }, {
     id: 4,
     name: 'Mike Harrington',
     setCount: 2,
     lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    face: 'img/profile_photos/mike.png'
   }];
 
   return {
