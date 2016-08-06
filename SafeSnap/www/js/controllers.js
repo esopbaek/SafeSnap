ab2d826a0059220d5fad8e0a7ac7fdcf2a870ccd
Esop Baek <e.baek@celmatix.com>
2016-08-06 02:15:47 -0400

    rename root namespace from starter to safeSnap, setUp fakeData, patient index view

--- SafeSnap/www/js/controllers.js
+++ SafeSnap/www/js/controllers.js
@@ -1,8 +1,8 @@
-angular.module('starter.controllers', [])
+angular.module('safeSnap.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Patients) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,14 +11,14 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
+  $scope.chats = Patients.all();
   $scope.remove = function(chat) {
-    Chats.remove(chat);
+    Patients.remove(patient);
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function($scope, $stateParams, Patients) {
+  $scope.chat = Patients.get($stateParams.patientId);
 })
 
 .controller('AccountCtrl', function($scope) {
--- SafeSnap/www/js/services.js
+++ SafeSnap/www/js/services.js
@@ -1,47 +1,52 @@
-angular.module('starter.services', [])
+angular.module('safeSnap.services', [])
 
-.factory('Chats', function() {
+.factory('Patients', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var patients = [{
     id: 0,
     name: 'Ben Sparrow',
+    setCount: 3,
     lastText: 'You on your way?',
     face: 'img/ben.png'
   }, {
     id: 1,
     name: 'Max Lynx',
+    setCount: 2,
     lastText: 'Hey, it\'s me',
     face: 'img/max.png'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
+    setCount: 4,
     lastText: 'I should buy a boat',
     face: 'img/adam.jpg'
   }, {
     id: 3,
     name: 'Perry Governor',
+    setCount: 1,
     lastText: 'Look at my mukluks!',
     face: 'img/perry.png'
   }, {
     id: 4,
     name: 'Mike Harrington',
+    setCount: 2,
     lastText: 'This is wicked good ice cream.',
     face: 'img/mike.png'
   }];
 
   return {
     all: function() {
-      return chats;
+      return patients;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(patient) {
+      patients.splice(patients.indexOf(patient), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(patientId) {
+      for (var i = 0; i < patients.length; i++) {
+        if (patients[i].id === parseInt(patientId)) {
+          return patients[i];
         }
       }
       return null;
